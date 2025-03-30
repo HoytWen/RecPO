4da1464b416a0790ecadf9e029aa79a17f653cf6
HoytWen <qianlongwen0118@gmail.com>
2025-03-30 16:45:07 -0400

    update command

--- multi_gpu_run.sh
+++ multi_gpu_run.sh
@@ -300,81 +300,81 @@
 #         --wandb_name Base-SimPO-gpu8 > log/simpo.log
 
 
- CUDA_VISIBLE_DEVICES=2,3,4,5,6,7,0,1 torchrun --nproc_per_node 8 --master_port=25641 sft.py \
-        --model_name Qwen/Qwen2.5-7B \
-        --train_dataset amazon-books_10000 \
-        --batch_size 2 \
-        --gradient_accumulation_steps 8 \
-        --prompt_path ./prompt/book_rating.txt \
-        --logging_dir log/ \
-        --output_dir output/ \
-        --learning_rate 1e-5 \
-        --num_train_epochs 5 \
-        --eval_step 0.2 \
-        --cutoff_len 1024 \
-        --report_to none \
-        --wandb_project RecPO \
-        --wandb_name Base-qwen-7B-SFT-gpu8 > log/base-sft
-
-
- CUDA_VISIBLE_DEVICES=2,3,4,5,6,7,0,1 torchrun --nproc_per_node 8 --master_port=25641 sft.py \
-        --model_name Qwen/Qwen2.5-7B \
-        --train_dataset beeradvocate_10000 \
-        --batch_size 2 \
-        --gradient_accumulation_steps 8 \
-        --prompt_path ./prompt/beer_rating.txt \
-        --logging_dir log/ \
-        --output_dir output/ \
-        --learning_rate 1e-5 \
-        --num_train_epochs 5 \
-        --eval_step 0.2 \
-        --cutoff_len 1024 \
-        --report_to none \
-        --wandb_project RecPO \
-        --wandb_name Base-qwen-7B-SFT-gpu8 > log/base-sft
-
-
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node 8 --master_port=25642 dpo.py \
-       --model_name Qwen/Qwen2.5-7B \
-       --resume_from_checkpoint output/amazon-books/Base-qwen-7B-SFT-gpu8/final_checkpoint/ \
-       --train_dataset amazon-books_10000 \
-       --batch_size 2 \
-       --gradient_accumulation_steps 16 \
-       --prompt_path ./prompt/book_rating.txt \
-       --logging_dir log/ \
-       --output_dir output/ \
-       --learning_rate 1e-5 \
-       --loss_type sigmoid \
-       --beta 1 \
-       --neg_num 1 \
-       --num_train_epochs 3 \
-       --prompt_cutoff_len 924 \
-       --cutoff_len 1024 \
-       --eval_step 0.2 \
-       --report_to none \
-       --wandb_project RecPO \
-       --wandb_name Base-qwen-7B-DPO-gpu4 > log/dpo.log
-
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node 8 --master_port=25642 dpo.py \
-       --model_name Qwen/Qwen2.5-7B \
-       --resume_from_checkpoint output/beeradvocate/Base-qwen-7B-SFT-gpu8/final_checkpoint/ \
-       --train_dataset beeradvocate_10000 \
-       --batch_size 2 \
-       --gradient_accumulation_steps 16 \
-       --prompt_path ./prompt/beer_rating.txt \
-       --logging_dir log/ \
-       --output_dir output/ \
-       --learning_rate 1e-5 \
-       --loss_type sigmoid \
-       --beta 1 \
-       --neg_num 1 \
-       --num_train_epochs 3 \
-       --prompt_cutoff_len 924 \
-       --cutoff_len 1024 \
-       --eval_step 0.2 \
-       --report_to none \
-       --wandb_project RecPO \
-       --wandb_name Base-qwen-7B-DPO-gpu4 > log/dpo.log
+# CUDA_VISIBLE_DEVICES=2,3,4,5,6,7,0,1 torchrun --nproc_per_node 8 --master_port=25641 sft.py \
+#        --model_name Qwen/Qwen2.5-7B \
+#        --train_dataset amazon-books_10000 \
+#        --batch_size 2 \
+#        --gradient_accumulation_steps 8 \
+#        --prompt_path ./prompt/book_rating.txt \
+#        --logging_dir log/ \
+#        --output_dir output/ \
+#        --learning_rate 1e-5 \
+#        --num_train_epochs 5 \
+#        --eval_step 0.2 \
+#        --cutoff_len 1024 \
+#        --report_to none \
+#        --wandb_project RecPO \
+#        --wandb_name Base-qwen-7B-SFT-gpu8 > log/base-sft
+#
+#
+# CUDA_VISIBLE_DEVICES=2,3,4,5,6,7,0,1 torchrun --nproc_per_node 8 --master_port=25641 sft.py \
+#        --model_name Qwen/Qwen2.5-7B \
+#        --train_dataset beeradvocate_10000 \
+#        --batch_size 2 \
+#        --gradient_accumulation_steps 8 \
+#        --prompt_path ./prompt/beer_rating.txt \
+#        --logging_dir log/ \
+#        --output_dir output/ \
+#        --learning_rate 1e-5 \
+#        --num_train_epochs 5 \
+#        --eval_step 0.2 \
+#        --cutoff_len 1024 \
+#        --report_to none \
+#        --wandb_project RecPO \
+#        --wandb_name Base-qwen-7B-SFT-gpu8 > log/base-sft
+#
+#
+#CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node 8 --master_port=25642 dpo.py \
+#       --model_name Qwen/Qwen2.5-7B \
+#       --resume_from_checkpoint output/amazon-books/Base-qwen-7B-SFT-gpu8/final_checkpoint/ \
+#       --train_dataset amazon-books_10000 \
+#       --batch_size 2 \
+#       --gradient_accumulation_steps 16 \
+#       --prompt_path ./prompt/book_rating.txt \
+#       --logging_dir log/ \
+#       --output_dir output/ \
+#       --learning_rate 1e-5 \
+#       --loss_type sigmoid \
+#       --beta 1 \
+#       --neg_num 1 \
+#       --num_train_epochs 3 \
+#       --prompt_cutoff_len 924 \
+#       --cutoff_len 1024 \
+#       --eval_step 0.2 \
+#       --report_to none \
+#       --wandb_project RecPO \
+#       --wandb_name Base-qwen-7B-DPO-gpu8 > log/dpo.log
+#
+#CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node 8 --master_port=25642 dpo.py \
+#       --model_name Qwen/Qwen2.5-7B \
+#       --resume_from_checkpoint output/beeradvocate/Base-qwen-7B-SFT-gpu8/final_checkpoint/ \
+#       --train_dataset beeradvocate_10000 \
+#       --batch_size 2 \
+#       --gradient_accumulation_steps 16 \
+#       --prompt_path ./prompt/beer_rating.txt \
+#       --logging_dir log/ \
+#       --output_dir output/ \
+#       --learning_rate 1e-5 \
+#       --loss_type sigmoid \
+#       --beta 1 \
+#       --neg_num 1 \
+#       --num_train_epochs 3 \
+#       --prompt_cutoff_len 924 \
+#       --cutoff_len 1024 \
+#       --eval_step 0.2 \
+#       --report_to none \
+#       --wandb_project RecPO \
+#       --wandb_name Base-qwen-7B-DPO-gpu8 > log/dpo.log
 
 
 #CUDA_VISIBLE_DEVICES=2,3,4,5,6,7,0,1 torchrun --nproc_per_node 8 --master_port=25641 rec_dpo.py \
@@ -428,51 +428,139 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node 8 --master_port=2
 #        --wandb_name Base-qwen-7B-RecDPO-ratio-ml2-gpu8 > log/recdpo
 
 
-CUDA_VISIBLE_DEVICES=2,3,4,5,6,7,0,1 torchrun --nproc_per_node 8 --master_port=25641 rec_dpo.py \
+#CUDA_VISIBLE_DEVICES=2,3,4,5,6,7,0,1 torchrun --nproc_per_node 8 --master_port=25641 rec_dpo.py \
+#        --model_name Qwen/Qwen2.5-7B \
+#        --resume_from_checkpoint output/beeradvocate/Base-qwen-7B-SFT-gpu8/final_checkpoint/ \
+#        --train_dataset beeradvocate_10000 \
+#        --batch_size 2 \
+#        --gradient_accumulation_steps 8 \
+#        --prompt_path ./prompt/beer_rating.txt \
+#        --logging_dir log/ \
+#        --output_dir output/ \
+#        --learning_rate 1e-5 \
+#        --loss_type sigmoid \
+#        --beta 1 \
+#        --margin_lambda 2 \
+#        --ratio True \
+#        --neg_num 3 \
+#        --num_train_epochs 3 \
+#        --eval_step 0.2 \
+#        --user_score True \
+#        --prompt_cutoff_len 924 \
+#        --cutoff_len 1024 \
+#        --report_to none \
+#        --wandb_project RecPO \
+#        --wandb_name Base-qwen-7B-RecDPO-ratio-ml2-gpu8 > log/recdpo
+#
+#
+#CUDA_VISIBLE_DEVICES=2,3,4,5,6,7,0,1 torchrun --nproc_per_node 8 --master_port=25641 rec_dpo.py \
+#        --model_name Qwen/Qwen2.5-7B \
+#        --resume_from_checkpoint output/amazon-books/Base-qwen-7B-SFT-gpu8/final_checkpoint/ \
+#        --train_dataset amazon-books_10000 \
+#        --batch_size 2 \
+#        --gradient_accumulation_steps 8 \
+#        --prompt_path ./prompt/book_rating.txt \
+#        --logging_dir log/ \
+#        --output_dir output/ \
+#        --learning_rate 1e-5 \
+#        --loss_type sigmoid \
+#        --beta 1 \
+#        --margin_lambda 2 \
+#        --ratio True \
+#        --neg_num 3 \
+#        --num_train_epochs 3 \
+#        --eval_step 0.2 \
+#        --user_score True \
+#        --prompt_cutoff_len 924 \
+#        --cutoff_len 1024 \
+#        --report_to none \
+#        --wandb_project RecPO \
+#        --wandb_name Base-qwen-7B-RecDPO-ratio-ml2-gpu8 > log/recdpo
+
+
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node 8 --master_port=25642 softmax_dpo.py \
         --model_name Qwen/Qwen2.5-7B \
-        --resume_from_checkpoint output/beeradvocate/Base-qwen-7B-SFT-gpu8/final_checkpoint/ \
-        --train_dataset beeradvocate_10000 \
+        --resume_from_checkpoint output/movielens/Base-qwen-7B-SFT-gpu8/final_checkpoint/ \
         --batch_size 2 \
+        --train_dataset movielens_10000 \
         --gradient_accumulation_steps 8 \
-        --prompt_path ./prompt/beer_rating.txt \
+        --prompt_path ./prompt/movie_rating.txt \
         --logging_dir log/ \
         --output_dir output/ \
-        --learning_rate 1e-5 \
+        --learning_rate 1e-5\
         --loss_type sigmoid \
         --beta 1 \
-        --margin_lambda 2 \
-        --ratio True \
         --neg_num 3 \
         --num_train_epochs 3 \
+        --prompt_cutoff_len 704 \
+        --cutoff_len 768 \
         --eval_step 0.2 \
-        --user_score True \
-        --prompt_cutoff_len 924 \
-        --cutoff_len 1024 \
         --report_to none \
         --wandb_project RecPO \
-        --wandb_name Base-qwen-7B-RecDPO-ratio-ml2-gpu8 > log/recdpo
+        --wandb_name Base-qwen-7B-SDPO-gpu8 > log/sdpo.log
 
 
-CUDA_VISIBLE_DEVICES=2,3,4,5,6,7,0,1 torchrun --nproc_per_node 8 --master_port=25641 rec_dpo.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node 8 --master_port=25642 softmax_dpo.py \
         --model_name Qwen/Qwen2.5-7B \
         --resume_from_checkpoint output/amazon-books/Base-qwen-7B-SFT-gpu8/final_checkpoint/ \
-        --train_dataset amazon-books_10000 \
         --batch_size 2 \
+        --train_dataset amazon-books_10000 \
         --gradient_accumulation_steps 8 \
         --prompt_path ./prompt/book_rating.txt \
         --logging_dir log/ \
         --output_dir output/ \
-        --learning_rate 1e-5 \
+        --learning_rate 1e-5\
         --loss_type sigmoid \
         --beta 1 \
-        --margin_lambda 2 \
-        --ratio True \
         --neg_num 3 \
         --num_train_epochs 3 \
-        --eval_step 0.2 \
-        --user_score True \
         --prompt_cutoff_len 924 \
         --cutoff_len 1024 \
+        --eval_step 0.2 \
         --report_to none \
         --wandb_project RecPO \
-        --wandb_name Base-qwen-7B-RecDPO-ratio-ml2-gpu8 > log/recdpo
+        --wandb_name Base-qwen-7B-SDPO-gpu8 > log/sdpo.log
+
+
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node 8 --master_port=25642 softmax_dpo.py \
+        --model_name Qwen/Qwen2.5-7B \
+        --resume_from_checkpoint output/steam/Base-qwen-7B-SFT-gpu8/final_checkpoint/ \
+        --batch_size 2 \
+        --train_dataset steam_10000 \
+        --gradient_accumulation_steps 8 \
+        --prompt_path ./prompt/game_rating.txt \
+        --logging_dir log/ \
+        --output_dir output/ \
+        --learning_rate 1e-5\
+        --loss_type sigmoid \
+        --beta 1 \
+        --neg_num 3 \
+        --num_train_epochs 3 \
+        --prompt_cutoff_len 924 \
+        --cutoff_len 1024 \
+        --eval_step 0.2 \
+        --report_to none \
+        --wandb_project RecPO \
+        --wandb_name Base-qwen-7B-SDPO-gpu8 > log/sdpo.log
+
+
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node 8 --master_port=25642 softmax_dpo.py \
+        --model_name Qwen/Qwen2.5-7B \
+        --resume_from_checkpoint output/beeradvocate/Base-qwen-7B-SFT-gpu8/final_checkpoint/ \
+        --batch_size 2 \
+        --train_dataset beeradvocate_10000 \
+        --gradient_accumulation_steps 8 \
+        --prompt_path ./prompt/beer_rating.txt \
+        --logging_dir log/ \
+        --output_dir output/ \
+        --learning_rate 1e-5\
+        --loss_type sigmoid \
+        --beta 1 \
+        --neg_num 3 \
+        --num_train_epochs 3 \
+        --prompt_cutoff_len 924 \
+        --cutoff_len 1024 \
+        --eval_step 0.2 \
+        --report_to none \
+        --wandb_project RecPO \
+        --wandb_name Base-qwen-7B-SDPO-gpu8 > log/sdpo.log
